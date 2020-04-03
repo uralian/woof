68279b0c457fb713896631e757860f02614274fa
Vlad Orzhekhovskiy <vlad@uralian.com>
2020-04-03 16:22:08 -0400

    Implement DataDog HTTP client

---
+++ src/it/scala/com/uralian/woof/AbstractITSpec.scala
@@ -0,0 +1,22 @@
+package com.uralian.woof
+
+import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds, Span}
+
+/**
+ * Base trait for integration test specifications.
+ */
+class AbstractITSpec extends Suite
+  with WordSpecLike
+  with MustMatchers
+  with BeforeAndAfterAll
+  with OptionValues
+  with ScalaFutures
+  with Inside {
+
+  /**
+   * Default timeout for Future testing.
+   */
+  implicit val defaultPatience = PatienceConfig(timeout = Span(5, Seconds), interval = Span(250, Millis))
+}
