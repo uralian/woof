8e6423a28c7bf5b4b4da3d35c0aa309b13ea6d31
Vlad Orzhekhovskiy <vlad@uralian.com>
2020-04-08 17:04:21 -0400

    Implement Retry feature for synchronous and asynchronous calls

--- src/test/scala/com/uralian/woof/AbstractUnitSpec.scala
+++ src/test/scala/com/uralian/woof/AbstractUnitSpec.scala
@@ -2,6 +2,7 @@ package com.uralian.woof
 
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds, Span}
 
 /**
  * Base trait for unit test specifications.
@@ -12,4 +13,15 @@ abstract class AbstractUnitSpec extends Suite
   with BeforeAndAfterAll
   with OptionValues
   with ScalaFutures
-  with Inside
+  with Inside {
+
+  /**
+   * Default timeout for Future testing.
+   */
+  implicit val defaultPatience = PatienceConfig(timeout = Span(10, Seconds), interval = Span(250, Millis))
+
+  /**
+   * @return the current time truncated to seconds as a java Instant.
+   */
+  def currentTime() = java.time.Instant.now.truncatedTo(java.time.temporal.ChronoUnit.SECONDS)
+}
